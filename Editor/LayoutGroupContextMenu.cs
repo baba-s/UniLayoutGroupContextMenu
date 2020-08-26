8b9e0665ede28d907b924a9a50c11d45feede8a9
baba_s <shotababa1206@gmail.com>
2020-08-26 09:19:17 +0900

    first commit

---
+++ Editor/LayoutGroupContextMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.Presets;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+	internal static class LayoutGroupContextMenu
+	{
+		private const string MENU_ITEM_NAME = "CONTEXT/LayoutGroup/Add ContentSizeFitter";
+
+		[MenuItem( MENU_ITEM_NAME )]
+		private static void AddContentSizeFitter( MenuCommand command )
+		{
+			var layoutGroup       = ( LayoutGroup ) command.context;
+			var gameObject        = layoutGroup.gameObject;
+			var contentSizeFitter = Undo.AddComponent<ContentSizeFitter>( gameObject );
+			var presets           = Preset.GetDefaultPresetsForObject( contentSizeFitter );
+
+			if ( presets == null || presets.Length <= 0 ) return;
+
+			var preset = presets[ 0 ];
+
+			preset.ApplyTo( contentSizeFitter );
+		}
+
+		[MenuItem( MENU_ITEM_NAME, true )]
+		private static bool CanAddContentSizeFitter( MenuCommand command )
+		{
+			var layoutGroup       = ( LayoutGroup ) command.context;
+			var contentSizeFitter = layoutGroup.GetComponent<ContentSizeFitter>();
+
+			return contentSizeFitter == null;
+		}
+	}
+}
